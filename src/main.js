2d59fb2fc013d5bf67aa5deeef025871ec71950e
hudan15 <2681439749@qq.com>
2020-03-13 10:10:35 +0800

    commit

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import App from './App'
+
+Vue.config.productionTip = false
+App.mpType = 'app'
+
+const app = new Vue(App)
+app.$mount()
+
+export default {
+  config: {
+    pages: [      
+      'pages/index/index',
+      'pages/topic/topic',
+      'pages/catalog/catalog',
+      'pages/cart/cart',
+      'pages/ucenter/index'
+    ],
+    window: {
+      backgroundTextStyle: 'light',
+      navigationBarBackgroundColor: '#fff',
+      navigationBarTitleText: '第一个小程序',
+      navigationBarTextStyle: 'black'
+    }
+  }
+}
